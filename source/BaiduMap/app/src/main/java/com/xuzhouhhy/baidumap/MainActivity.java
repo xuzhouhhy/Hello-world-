86e0ad0ac3ca33bc10405cac4ca339e99a75a8bb
hanhongyun <hongyun_han@huacenav.com>
2017-08-02 19:48:39 +0800

    显示百度地图

--- source/BaiduMap/app/src/main/java/com/xuzhouhhy/baidumap/MainActivity.java
+++ source/BaiduMap/app/src/main/java/com/xuzhouhhy/baidumap/MainActivity.java
@@ -3,11 +3,14 @@ package com.xuzhouhhy.baidumap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.baidu.mapapi.SDKInitializer;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
     }
 }
