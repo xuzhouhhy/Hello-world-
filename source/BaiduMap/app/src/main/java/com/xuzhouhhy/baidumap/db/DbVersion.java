a2b3f40b1c6b354e62506ea2b17c7adb948788b6
hanhongyun <hongyun_han@huacenav.com>
2017-08-06 13:59:15 +0800

    数据库版本枚举

--- source/BaiduMap/app/src/main/java/com/xuzhouhhy/baidumap/db/DbVersion.java
+++ source/BaiduMap/app/src/main/java/com/xuzhouhhy/baidumap/db/DbVersion.java
@@ -4,8 +4,25 @@ package com.xuzhouhhy.baidumap.db;
  * Created by hanhongyun on 2017/8/4.
  */
 
-public enum  DbVersion {
+public enum DbVersion {
 
-    VERSION_721;
+    VERSION_721(721);
+
+    private int mValue;
+
+    public int getValue() {
+        return mValue;
+    }
+
+    DbVersion(int value) {
+        mValue = value;
+    }
+
+    /**
+     * Returns 当前版本号
+     */
+    public static DbVersion current() {
+        return VERSION_721;
+    }
 
 }
